99b8b883a661035069f407ee5356d28c0e6ff17e
bluebearrii <thananan.math.acs@gmail.com>
2021-10-23 17:33:06 +0700

    app: Set dynamic header and footer global layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { Provider } from "react-redux";
+import styled from "styled-components";
+import { Footer } from "./components/footer";
+import { CustomNavbar } from "./components/navbar";
 import { Products } from "./components/products";
+import store from "./redux/store/store";
 
 function App() {
   return (
-    <Products />
+    <Provider store={store}>
+      <Wrapper>
+        <CustomNavbar />
+        <Main>
+          <Products />
+        </Main>
+        <Footer />
+      </Wrapper>
+    </Provider>
   );
 }
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  padding-top: 70px;
+`;
+
+const Main = styled.div`
+  flex: 1;
+`;
 
 export default App;
